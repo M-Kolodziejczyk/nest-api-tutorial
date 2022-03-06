a6fe36bbc1ee5ddbb6e2cfa5d7260852e1abe18c
Michal <kolodziejczykmichal269@gmail.com>
2022-03-06 14:20:29 +0100

    auth logic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Apap');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
